38286c45958ae1e11ca225c906b6307dda42b4cb
katoari <anselmood06@gmail.com>
2023-01-18 22:09:52 +0800

    Initial Commit

---
+++ DTN Manila Parking Manager/DTN_Manila_Parking_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DTN_Manila_Parking_ManagerApp.swift
+//  DTN Manila Parking Manager
+//
+//  Created by Anselmo Oduca on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct DTN_Manila_Parking_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
